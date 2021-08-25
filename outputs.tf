c556639ad82c0b49f0632ee4025772635361f467
Venkatesh <nvenkatesh.27@gmail.com>
2021-08-25 15:02:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 // This allows schematics_workspace data to be referenced by other resources and the terraform CLI
 // Modify this if only certain data should be exposed
-output "ibm_schematics_workspace" {
-  value       = ibm_schematics_workspace.schematics_workspace_instance
+output "ibm_schematics_workspace_id" {
+  value       = ibm_schematics_workspace.schematics_workspace_instance.ibm_schematics_workspace.id
   description = "schematics_workspace resource instance"
 }
